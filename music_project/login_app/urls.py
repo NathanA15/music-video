f9dc40d82544f509b2e81fa1e3aa5256ddcdb830
Nathan Adda <nathanadda@me.com>
2018-12-14 14:50:32 +0200

    login...

--- music_project/login_app/urls.py
+++ music_project/login_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'login_app'
 
 urlpatterns = [
-	
+	path('', views.index, name='index_login'),
+	path('signup/', views.signup, name='signup'),
+	path('login/', views.log_in, name='loginpage'),
+	path('logging_out/', views.logging_out, name='logging_out'),
 ]
