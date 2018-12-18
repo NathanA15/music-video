b0d7211860f58fd7aac979d11a2aabc24152ac8f
Theo Nataf <tdh.nataf@gmail.com>
2018-12-18 10:54:26 +0200

    comments form plus view

--- music_project/comment_app/views.py
+++ music_project/comment_app/views.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from profile_app.models import UserProfileInfo
+from video_app.models import Video
 
 # Create your views here.
+def write_comment(request, video_id):
+	user = request.user 
+	if not user.is_authenticated:
+		user.username = 'Anonymous User'
+	
+	if request.method == 'POST':
+		userprofileinfo = UserProfileInfo.objects.get(user=user)
+		text = request.POST.get('text')
+		video = Video.objects.get(video_id=video_id)
+		comment = Comment(text=text, userprofileinfo = userprofileinfo, video=video)
+		comment.save()
+
+		response_data = {
+			'result': 'success',
+			'id': comment.id,
+			'text': comment.text,
+			'userprofileinfo': comment.userprofileinfo,
+			'date': comment.date,
+			'video': comment.video,
+		}
+
+		return return JsonResponse(response_data)
+
+	else:
+		error = {'error': 'Non POST method not allowed'}
+		return JsonResponse(error)
+		
