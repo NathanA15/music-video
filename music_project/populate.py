77fa0afbaea7ef6fa84a3491206f5e108aade425
Raphael Zeitoun <raph_zeitoun@hotmail.com>
2018-12-18 09:39:50 +0200

    populate playlists and videos

---
+++ music_project/populate.py
@@ -0,0 +1,50 @@
+import requests
+import json
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_project.settings')
+import django
+django.setup()
+from video_app.models import Playlist,Video
+
+api_key = 'AIzaSyBiqnvo8w7-EhK0lT_TxQoREtFm42nyW4o'
+channel_id = 'UCPVhZsC2od1xjGhgEc2NEPQ'
+
+playlists_api_url = 'https://www.googleapis.com/youtube/v3/search?key={}&channelId={}&part=snippet,id&order=date&maxResults={}'.format(api_key, channel_id, 20)
+
+
+
+def get_videos(playlist):
+	
+	playlist_api_url= 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults={}&playlistId={}&key={}'.format(20,playlist.playlist_id, api_key)
+	videos = requests.get(playlist_api_url).json()
+
+	for video in videos['items']:
+		video_id = video['id']
+		title = video['snippet']['title']
+		description = video['snippet']['description']
+		thumbnail_url = video['snippet']['thumbnails']['default']['url']
+
+		Video.objects.get_or_create(title= title, video_id=video_id, description=description
+			, thumbnail_url=thumbnail_url, playlist=playlist)[0]
+			
+
+
+def get_playlists(api_url):
+	videos = requests.get(api_url).json()
+	
+	for dic in videos['items']:
+		if 'playlistId' in dic['id']:
+			playlist_id = dic['id']['playlistId']
+			title = dic['snippet']['title']
+			description = dic['snippet']['description']
+			thumbnail_url = dic['snippet']['thumbnails']['default']['url']
+
+			playlist = Playlist.objects.get_or_create(playlist_id=playlist_id, title=title, description=description, thumbnail_url=thumbnail_url)[0]
+			get_videos(playlist)
+
+
+if __name__ == '__main__':
+    print('Staring to populate...')
+    get_playlists(playlists_api_url)
+    print('Finished populating!')
+
--- music_project/video_app/admin.py
+++ music_project/video_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from video_app.models import Video
+from video_app.models import Video, Playlist
 
 
 
 # Register your models here.
 admin.site.register(Video)
+admin.site.register(Playlist)
