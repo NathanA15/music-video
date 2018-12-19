97c772262aa5ade4bc38051f1a41a8ef20692076
Nathan Adda <nathanadda@me.com>
2018-12-19 10:34:17 +0200

    playlist-page/homepage

--- music_project/video_app/urls.py
+++ music_project/video_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'video_app'
 
 urlpatterns = [
+	path('', views.homepage, name='homepage'),
+	path('playlist_page/<playlist_id>/', views.playlist_page, name='playlist_page'),
 ]
--- music_project/video_app/views.py
+++ music_project/video_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from video_app.models import Playlist, Video
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def homepage(request):
+	playlists = Playlist.objects.all()
+	last_videos = Video.objects.order_by('id')[0:20]
+
+	return render(request, 'homepage.html', context ={
+			'playlists': playlists,
+			'videos': last_videos
+		})
+
+
+def playlist_page(request, playlist_id):
+	print('###############')
+	# playlist = Playlist.objects.get(playlist_id=playlist_id)
+	playlist_videos = Video.objects.filter(playlist__playlist_id=playlist_id)
+
+	return render(request, 'playlist_page.html', context={'videos':playlist_videos, 'playlist': Playlist.objects.get(playlist_id=playlist_id)})
