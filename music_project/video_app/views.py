ab372a498f4b5629f501a7cf8352a2d90fb3b1db
Raphael Zeitoun <raph_zeitoun@hotmail.com>
2018-12-19 11:37:01 +0200

    video page

--- music_project/video_app/views.py
+++ music_project/video_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from video_app.models import Playlist, Video
 
+
+def get_all_video_from_playlist(playlist_id):
+	videos = Video.objects.filter(playlist__playlist_id = playlist_id)
+	return videos
+
 # Create your views here.
 
 def index(request):
@@ -8,4 +13,7 @@ def index(request):
 
 def get_video(request, video_id):
 	video = Video.objects.get(video_id= video_id)
-	return render(request, 'video_page.html', {'video':video} )
+	videos_from_playlist = get_all_video_from_playlist(video.playlist.playlist_id)
+	return render(request, 'video_page.html', {'video':video, 'all_videos':videos_from_playlist} )
+
+
